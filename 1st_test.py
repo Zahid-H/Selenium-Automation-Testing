8a630ef41f82e995a941b675296c6d53ee144aff
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-03-30 15:07:27 +0600

    first selenium setup done

---
+++ 1st_test.py
@@ -0,0 +1,9 @@
+import selenium
+
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="G:\\chromedriver.exe")
+
+driver.get("https://www.youtube.com/@accidentalsoftwaretester")
+
