2c11021ca0a2cfb29610766c744db7d0dea82b1f
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-03-31 20:49:07 +0600

    google serach bar auto mation

--- 1st_test.py
+++
@@ -1,9 +0,0 @@
-import selenium
-
-
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path="G:\\chromedriver.exe")
-
-driver.get("https://www.youtube.com/@accidentalsoftwaretester")
-
---
+++ google serach bar Automation.py
@@ -0,0 +1,13 @@
+import selenium
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="E:\chromedriver.exe")
+
+driver.get("https://www.google.com/")
+
+driver.find_element_by_name("q").send_keys("Admin")
+
+
+
+
