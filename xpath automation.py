4746a4036c356af4254e0aec5aaf178e34e28fbf
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-04-01 22:26:33 +0600

    xpath.text

--- xpath automation.py
+++ xpath automation.py
@@ -9,7 +9,10 @@ driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
 #Xpath the most important locators
 # //tagname[@attribute = ‘value’]
 #<input> is tag name,@attribute is value, and ='value' is radio1
-driver.find_element_by_xpath("//input[@value='radio1']").click()
+#driver.find_element_by_xpath("//input[@value='radio1']").click()
+#//tagname[text() = ‘type text here’]
 
+#for getting the name with Xpath
+a= driver.find_element_by_xpath("//legend[text()='Checkbox Example']").text
 
-
+print(a)
