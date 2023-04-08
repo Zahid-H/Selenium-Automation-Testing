42ffea5a8d9e2b838e83d1f5c992da14eba166ca
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-04-08 14:14:49 +0600

    get attribute

--- get attribute.py
+++ get attribute.py
@@ -2,3 +2,9 @@ import selenium
 import time
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path="E:\chromedriver.exe")
+
+driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
+
+get_attribute = driver.find_element_by_xpath("//input[@id='name']").get_attribute("placeholder")
+
+print(get_attribute)
